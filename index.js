a9bfd1be956a1175ad4307a4cbd044a51d5a4ba9
Dan Fessler <fezzinate@gmail.com>
2019-04-02 00:22:21 -0700

    passing in botId to the Royale

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ bot.on("message", msg => {
         if (games[msg.channel]) games[msg.channel].stop();
         games[msg.channel] = new Royale(
           bot,
+          botId,
           channelName,
           command[1] !== undefined ? command[1] : defaults.initialTime,
           command[2] !== undefined ? command[2] : defaults.loop
